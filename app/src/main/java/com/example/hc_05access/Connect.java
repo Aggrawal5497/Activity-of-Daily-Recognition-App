6d09976d68dd5a754a1edc4aa0edbf22d96cf3ff
Sahil Aggrawal <agrawal5apr@gmail.com>
2019-03-07 21:45:03 +0530

    Created AsyncTask to connect to Bluetooth instead of normal thread

--- app/src/main/java/com/example/hc_05access/Connect.java
+++ app/src/main/java/com/example/hc_05access/Connect.java
@@ -3,7 +3,9 @@ package com.example.hc_05access;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +16,7 @@ import android.widget.Toast;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
+import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 
@@ -21,6 +24,7 @@ public class Connect extends AppCompatActivity {
     BluetoothDevice bt;
     BluetoothAdapter btAdap;
     TextView txtdevice, txtaddr;
+    private boolean connected = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,27 +33,78 @@ public class Connect extends AppCompatActivity {
         btAdap = BluetoothAdapter.getDefaultAdapter();
         Set<BluetoothDevice> pairedDevices = btAdap.getBondedDevices();
 
-        txtdevice = (TextView) findViewById(R.id.txtName);
-        txtaddr = (TextView) findViewById(R.id.txtAddress);
+        txtdevice = findViewById(R.id.txtName);
+        txtaddr = findViewById(R.id.txtAddress);
         Intent intent = getIntent();
 
         String device_name = intent.getStringExtra("Name");
         String device_address = intent.getStringExtra("Address");
-        int pos = intent.getExtras().getInt("Position");
+        int pos = Objects.requireNonNull(intent.getExtras()).getInt("Position");
 
         for(BluetoothDevice b : pairedDevices){
             if(b.getName().equals(device_name))
                 bt = b;
         }
 
-        Toast.makeText(this, "position" + pos, Toast.LENGTH_LONG).show();
         txtdevice.setText(device_name);
         txtaddr.setText(device_address);
 
     }
 
     public void onConnect(View view){
+        ConnectThreadAsync task = new ConnectThreadAsync(bt, btAdap, this);
+        task.execute();
+        /*
         ConnectThread t = new ConnectThread(bt, this, btAdap);
         t.start();
+        */
+    }
+
+    private class ConnectThreadAsync extends AsyncTask<Void, Void, Boolean> {
+        private final UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+        private BluetoothSocket mmSocket;
+        private Context mContext;
+        private BluetoothAdapter b;
+        private boolean flag = false;
+
+        ConnectThreadAsync(BluetoothDevice device, BluetoothAdapter bt, Context context){
+            BluetoothSocket tmp = null;
+            try {
+                tmp = device.createInsecureRfcommSocketToServiceRecord(uuid);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mmSocket = tmp;
+            mContext = context;
+            b = bt;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            b.cancelDiscovery();
+            try {
+                mmSocket.connect();
+                flag = true;
+            } catch (IOException e) {
+                try {
+                    mmSocket.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            return flag;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            if(aBoolean){
+                connected = true;
+                Toast.makeText(mContext, "Connected", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                connected = false;
+                Toast.makeText(mContext, "Not Connected", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
