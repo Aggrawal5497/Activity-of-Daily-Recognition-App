9edd9628d9c11244919125b11ad7928ca0ebb60c
Sahil Aggrawal <agrawal5apr@gmail.com>
2019-03-16 14:01:31 +0530

    Data Preparation module for feeding into tensorflow model added

--- app/src/main/java/com/example/hc_05access/BluetoothCommunicate.java
+++ app/src/main/java/com/example/hc_05access/BluetoothCommunicate.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.logging.Handler;
 
 public class BluetoothCommunicate extends Thread {
@@ -22,12 +23,18 @@ public class BluetoothCommunicate extends Thread {
     private byte[] mmBuffer;
     private boolean run = true;
     private Activity mActivity;
-    private TextView disp;
+    private TextView disp1, disp2, disp3, disp4;
+    private DataPreperation<Integer[]> dp;
 
     BluetoothCommunicate(BluetoothSocket socket, Context context){
+
+        dp = new DataPreperation<Integer[]>(2);
         mmSocket = socket;
         mActivity = (Activity) context;
-        disp = mActivity.findViewById(R.id.txtRecv);
+        disp1 = (TextView) mActivity.findViewById(R.id.txt1);
+        disp2 = (TextView) mActivity.findViewById(R.id.txt2);
+        disp3 = (TextView) mActivity.findViewById(R.id.txt3);
+        disp4 = (TextView) mActivity.findViewById(R.id.txtMain);
 
         try {
             mmInStream = mmSocket.getInputStream();
@@ -55,10 +62,23 @@ public class BluetoothCommunicate extends Thread {
                     if (numBytes == 9) {
                         str = new String(mmBuffer, StandardCharsets.UTF_8);
                         final String finalStr = str;
+                        final String[] val = finalStr.split(" ");
+                        final Integer[] arr = new Integer[val.length];
+                        int i = 0;
+                        for(String s : val){
+                            arr[i] = Integer.parseInt(s.trim());
+                            i++;
+                        }
+                        dp.add(arr);
                         mActivity.runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                disp.setText(finalStr);
+                                disp1.setText(arr[0] + "");
+                                disp2.setText(arr[1] + "");
+                                disp3.setText(arr[2] + "");
+                                if(dp.size() == 2){
+                                    disp4.setText(Arrays.deepToString(get2dArray(dp)));
+                                }
                             }
                         });
                         Log.d("READ", str);
@@ -93,5 +113,20 @@ public class BluetoothCommunicate extends Thread {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private Integer[][] get2dArray(DataPreperation<Integer[]> val){
+        Integer[][] arr = new Integer[val.size()][3];
+        int i = 0;
+        for(Integer[] a : val) {
+            int j = 0;
+            for(Integer z : a) {
+                arr[i][j] = z;
+                j++;
+            }
+            i++;
+        }
+        return arr;
+
     }
 }
---
+++ app/src/main/java/com/example/hc_05access/DataPreperation.java
@@ -0,0 +1,20 @@
+package com.example.hc_05access;
+
+import java.util.ArrayList;
+
+public class DataPreperation<K> extends ArrayList<K> {
+    private int maxSize;
+
+    DataPreperation(int size){
+        this.maxSize = size;
+    }
+
+    @Override
+    public boolean add(K k){
+        boolean r = super.add(k);
+        if(size() > this.maxSize){
+            removeRange(0, size() - maxSize);
+        }
+        return r;
+    }
+}
